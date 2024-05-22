8b724ff71d4af92e97f18c572b406f709bda2dcc
sillygeniuss <sillygenius@outlook.com>
2024-05-22 07:26:09 -0700

    feature: section_03 complete

--- exercises/02_basic_calculator/08_overflow/src/lib.rs
+++ exercises/02_basic_calculator/08_overflow/src/lib.rs
@@ -6,10 +6,8 @@
 // at the root of the repository, not in the `Cargo.toml` of the exercise.
 
 pub fn factorial(n: u32) -> u32 {
-    let mut result = 1;
-    for i in 1..=n {
-        result *= i;
-    }
+    let mut result: u32= 1;
+    (1..=n).into_iter().for_each(|x| {result = result.wrapping_mul(x);});
     result
 }
 
